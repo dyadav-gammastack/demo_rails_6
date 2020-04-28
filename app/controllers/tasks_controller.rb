d3764656592bd3ea8f93a096726e9ab2d8b57f1b
dyadav-gammastack <57951513+dyadav-gammastack@users.noreply.github.com>
2020-04-28 17:35:20 +0530

    Revert "Add feature to create Task"
    This reverts commit 1fedd10af57e56c03dd2c80761976885b25b1b0e.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,24 +1,9 @@
 class TasksController < ApplicationController
-  def new
-    @task = Task.new
-  end
-
-  def create
-    Task.create(task_params)
-    redirect_to tasks_path
-  end
-
   def index
     @tasks = Task.includes(:comments)
   end
 
   def show
     @task = Task.includes(:comments).first
-  end
-
-  private
-
-  def task_params
-    params.require(:task).permit(:name)
   end
 end
