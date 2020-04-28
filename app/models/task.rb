ffe13bf77050642044549915127bfce38c95377e
Deepak Yadav <dyadav@grepruby.io>
2020-04-28 20:15:00 +0530

    Added nested form fields

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ApplicationRecord
   has_rich_text :content
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
+  accepts_nested_attributes_for :comments, allow_destroy: true, reject_if: :all_blank
 end
